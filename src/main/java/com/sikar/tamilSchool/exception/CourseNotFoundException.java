a82ce9e8bcf578dddd6e44a167d75f5009f07cd7
elumalai <mynameiselumalai@gmail.com>
2024-09-08 17:20:20 -0400

    Removes Course related files, separated into course micro service

--- src/main/java/com/sikar/tamilSchool/exception/CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sikar.tamilSchool.exception;
-
-public class CourseNotFoundException extends RuntimeException {
-    public CourseNotFoundException(String message) {
-        super(message);
-    }
-}
